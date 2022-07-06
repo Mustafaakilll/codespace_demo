cdb4a9b2d1d83eec431bae87c980ff98cad2f79b
Mustafa Akil <37738604+Mustafaakilll@users.noreply.github.com>
2022-07-06 13:29:15 +0000

    Demo change

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-let express = require('express');
+let express = require("express");
 let app = express();
-let ejs = require('ejs');
-const haikus = require('./haikus.json');
+let ejs = require("ejs");
+const haikus = require("./haikus.json");
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'))
-app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-  res.render('index', {haikus: haikus});
+app.get("/", (req, res) => {
+  res.render("index", { haikus: haikus });
+});
+
+app.get("/add", (req, res) => {
+  res.send({
+    text: "Hello WORLD",
+  });
 });
 
 app.listen(port);
